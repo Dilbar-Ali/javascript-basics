72879d4a1418957f392fa18f81dbd04f0f3e4fc7
Dilbar-Ali <crazydevelopers001@gmail.com>
2022-10-02 14:08:30 -0700

    it is one the way

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log('Hello Developers ');// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Welcome to Programiz!");
+console.log("Hi");
+document.write("i am the best");
+
+
+let a=5;
+let b=6;
+let x="Ali";
+console.log(a+b);
+
+let c=a+b;
+document.write("The sum of A and B  "+ c);
+
+console.log(typeof(x));
+
+let fname="Ali";
+let lname="Khan3";
+console.log(fname+lname);
